bfe253a797a85672f408dedefd715ee3990a88ff
reivincitot <reivincitot@gmail.com>
2023-02-28 01:24:52 -0300

    Video 7 render template modulo 1 mi primera aplicacion web con python y flask

--- app/app.py
+++ app/app.py
@@ -1,21 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-#Metodo 1 de creacion de rutas de la aplicacion flask
+# Metodo 1 de creacion de rutas de la aplicacion flask
 
 """@app.route('/')
 def index():
-    return 'CódigoFacilito'"""
+    return 'Código Facilito'"""
 
-#metodo 2 de creacion de rutasd de la aplicacion flask
+# metodo 2 de creacion de rutasd de la aplicacion flask
+
+
 def index():
-    return 'CódigoFacilito'
+    #return render_template('/index.html', titulo='Index') 
+    data = {
+        'titulo' : 'Index',
+        'encabezado':'Bienvenido'
+    }
 
 def hola_mundo():
     return 'hola mundo!'
 
-if __name__=='__main__':
-    app.add_url_rule('/hola_mundo', view_func = hola_mundo)
-    app.add_url_rule('/',view_func=index)
+
+if __name__ == '__main__':
+    app.add_url_rule('/hola_mundo', view_func=hola_mundo)
+    app.add_url_rule('/', view_func=index)
     app.run(debug=True)
