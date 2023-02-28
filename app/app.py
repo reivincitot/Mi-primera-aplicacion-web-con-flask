7403701dfcf0c75fd1a3b9109362944f3001f2cf
reivincitot <reivincitot@gmail.com>
2023-02-28 01:02:31 -0300

    Vide 6 primera pagina web y hola mundo modulo 1 mi primera aplicacion web con python y flask codigo facilito

--- app/app.py
+++ app/app.py
@@ -2,5 +2,20 @@ from flask import Flask
 
 app = Flask(__name__)
 
+#Metodo 1 de creacion de rutas de la aplicacion flask
+
+"""@app.route('/')
+def index():
+    return 'CódigoFacilito'"""
+
+#metodo 2 de creacion de rutasd de la aplicacion flask
+def index():
+    return 'CódigoFacilito'
+
+def hola_mundo():
+    return 'hola mundo!'
+
 if __name__=='__main__':
-    app.run()
+    app.add_url_rule('/hola_mundo', view_func = hola_mundo)
+    app.add_url_rule('/',view_func=index)
+    app.run(debug=True)
